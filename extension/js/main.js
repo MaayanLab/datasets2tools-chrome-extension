2e5ecb8f67317fadf6db225630d0ab5fddff5f7a
denis-torre <denis.torre@mssm.edu>
2017-02-02 13:46:28 -0500

    Code overhaul, added GEO and DataMed landing page support

--- extension/js/main.js
+++ extension/js/main.js
@@ -6,11 +6,44 @@
 
 function main() {
 	// Load on DataMed and GEO
-	if (Page.isDataMed() || Page.isGEO()) {
-		datasets2toolsMain();
-	} else if (Page.isLDP()) {
-		datasets2toolsLincsMain();
+	// if (Page.isDataMed() || Page.isGEO()) {
+	// 	datasets2toolsMain();
+	// } else if (Page.isLDP()) {
+	// 	datasets2toolsLincsMain();
+	// }
+
+
+	if (Page.isDatasetSearchResults()) { 
+		// Get parents
+		var $parents = Interface2.locateParents();
+
+		// Get canned analyses
+		var cannedAnalysisData = API.main($parents);
+
+		// Add toolbars
+		Interface2.addCannedAnalyses($parents, cannedAnalysisData);
+
+		// Add event listeners
+		eventListener.main(cannedAnalysisData);
+
+		console.log(cannedAnalysisData);
+		
+	} else if (Page.isDatasetLanding()) {
+		// Get parent
+		var $parent = Interface2.locateParent();
+		
+		// Get canned analyses
+		var cannedAnalysisData = API.main($parent);
+
+		// Add toolbar
+		Interface2.addCannedAnalyses($parent, cannedAnalysisData);
+
+		// Add event listeners
+		eventListener.main(cannedAnalysisData);
+		
+		console.log(cannedAnalysisData);
 	}
+
 }
 
 
